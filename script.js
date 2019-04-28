f55c6db6dcf0d02d7a88ab497595d7cb9b6f3629
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-04-28 19:52:47 +0300

    Modify & add img to script.html

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-var element = 
+var movies = [
+    {
+      id: 1,
+      title: 'Harry Potter',
+      desc: 'film o czarodzieju',
+      img: 'http://via.placeholder.com/400x200'
+    },
+    {
+      id: 2,
+      title: 'Król Lew',
+      desc: 'Film o królu sawanny',
+      img: 'http://via.placeholder.com/400x200'
+    },
+    {
+      id: 3,
+      title: 'W pogoni za szczesciem',
+      desc: 'Co jest wazne w zyciu..pieniadze?',
+      img: 'http://via.placeholder.com/400x200'
+    },
+    {
+      id: 4,
+      title: 'Me before you',
+      desc: 'Eutanazja..., czy kalectwo to dobry powod by sie decydowac na ten krok?',
+      img: 'http://via.placeholder.com/400x200'
+    }
+  ];
+
+  var moviesElements = movies.map(function(movie) {
+    return React.createElement('li', {key: movie.id},
+        React.createElement('h2', {}, movie.title),
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {src: movie.img})
+      );
+  });
+
+  var element = 
     React.createElement('div', {},
       React.createElement('h1', {}, 'Lista filmow'),
-      React.createElement('ul', {},
-        React.createElement('li', {},
-          React.createElement('h2', {}, 'Harry Potter'),
-          React.createElement('p', {}, 'Film o czarodzieju'),
-        ),
-        React.createElement('li', {},
-          React.createElement('h2', {}, 'Krol lew'),
-          React.createElement('p', {}, 'Film o malym lwie'),
-        )
-      )
+      React.createElement('ul', {}, moviesElements)
     );
-    
-ReactDOM.render(element, document.getElementById('app'));
-    
+
+  ReactDOM.render(element, document.getElementById('app'));
